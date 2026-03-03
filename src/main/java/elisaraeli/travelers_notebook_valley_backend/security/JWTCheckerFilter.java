df6f5c236c6aaf0d5a7ede4ad86d9a9ee400e788
Elisa Raeli <79461498+Elisa-Ra@users.noreply.github.com>
2026-03-03 15:24:42 +0100

    adds security config + JWT

---
+++ src/main/java/elisaraeli/travelers_notebook_valley_backend/security/JWTCheckerFilter.java
@@ -0,0 +1,51 @@
+package elisaraeli.travelers_notebook_valley_backend.security;
+
+
+import elisaraeli.travelers_notebook_valley_backend.entities.Utente;
+import elisaraeli.travelers_notebook_valley_backend.exceptions.UnauthorizedException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JWTCheckerFilter extends OncePerRequestFilter {
+    private JWTTools jwtTools;
+    private UtenteService utenteService;
+
+    @Autowired
+    public JWTCheckerFilter(JWTTools jwtTools, UtenteService utenteService) {
+        this.jwtTools = jwtTools;
+        this.utenteService = utenteService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer "))
+            throw new UnauthorizedException("La chiave d'accesso non è valida.");
+        String accessToken = authHeader.replace("Bearer ", "");
+        jwtTools.verifyToken(accessToken);
+
+        // verify authorization
+        Utente authUtente = this.utenteService.findById(jwtTools.getId(accessToken));
+        Authentication authentication = new UsernamePasswordAuthenticationToken(authUtente, null, authUtente.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        //*************************************
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return new AntPathMatcher().match("/auth/**", request.getServletPath());
+    }
+}
