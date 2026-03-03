07edb33e020fcb54f93774496f2dbdca4e5cd370
Elisa Raeli <79461498+Elisa-Ra@users.noreply.github.com>
2026-03-03 12:00:48 +0100

    adds entity Conferita

---
+++ src/main/java/elisaraeli/travelers_notebook_valley_backend/entities/Conferita.java
@@ -0,0 +1,38 @@
+package elisaraeli.travelers_notebook_valley_backend.entities;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "conferite")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Conferita {
+    @Id
+    @GeneratedValue
+    @Setter(AccessLevel.NONE)
+    private UUID id;
+
+    @Column(nullable = false)
+    private LocalDate dataConferimento = LocalDate.now();
+
+    @ManyToOne
+    @JoinColumn(name = "id_medaglia", nullable = false)
+    private Medaglia medaglia;
+
+    @ManyToOne
+    @JoinColumn(name = "id_utente", nullable = false)
+    private Utente utente;
+
+    public Conferita(Medaglia medaglia, Utente utente) {
+        this.medaglia = medaglia;
+        this.utente = utente;
+    }
+}
