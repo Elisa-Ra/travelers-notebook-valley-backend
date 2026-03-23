0279c03dd424355e642392d9403ec1ec08ba8d78
Elisa Raeli <79461498+Elisa-Ra@users.noreply.github.com>
2026-03-23 16:24:27 +0100

    Update WebConfig.java

--- src/main/java/elisaraeli/travelers_notebook_valley_backend/config/WebConfig.java
+++ src/main/java/elisaraeli/travelers_notebook_valley_backend/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
+                        .allowedOriginPatterns(
                                 "http://localhost:5173",
                                 "https://travelers-notebook-valley-frontend.vercel.app"
                         )
